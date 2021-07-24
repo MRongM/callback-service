45ff2c4cfd9b773786775b0cdd7a201c3c05f91e
MRongM <idskof@sina.cn>
2021-07-24 17:34:13 +0800

    service adj

---
+++ services/blog.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+def apply(data):
+        res = {}
+
+        if data:
+                os.system("bash /root/MRongM/update.sh")
+                res["msg"]="ok"
+        return res
+
